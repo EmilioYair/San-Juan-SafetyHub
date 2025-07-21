9f4878bdbdd1a0fc2efa6599401e85be242202a8
EmilioYair <156270203+EmilioYair@users.noreply.github.com>
2025-07-21 09:34:08 -0600

    Add files via upload

---
+++ datos_mapa.js
@@ -0,0 +1,43 @@
+// Archivo: datos_mapa.js
+const incidentes = [
+    {
+        lat: 20.3925, lng: -99.9850, intensidad: 1.0,
+        zona: "Santa Cruz Nieto",
+        detalle: "Reporte de homicidio en Av. Central."
+    },
+    {
+        lat: 20.3800, lng: -99.9810, intensidad: 1.0,
+        zona: "Fundadores",
+        detalle: "Reporte de homicidio en la zona."
+    },
+    {
+        lat: 20.3000, lng: -99.9500, intensidad: 0.9,
+        zona: "Palmillas",
+        detalle: "Reporte de homicidio en construcción."
+    },
+    {
+        lat: 20.3850, lng: -99.9950, intensidad: 0.6,
+        zona: "Zona Centro",
+        detalle: "Reportes de robos a transeúntes y riñas."
+    },
+    {
+        lat: 20.3890, lng: -99.9570, intensidad: 0.6,
+        zona: "Zona Oriente",
+        detalle: "Reportes de detenciones por delitos contra la seguridad."
+    },
+    {
+        lat: 20.388, lng: -99.954, intensidad: 0.6,
+        zona: "Zona Oriente",
+        detalle: "Mega operativo con 3 detenidos y aseguramiento de inmuebles."
+    },
+    {
+        lat: 20.4000, lng: -100.0000, intensidad: 0.5,
+        zona: "Los Nogales / Espíritu Santo",
+        detalle: "Reportes de riñas con heridos."
+    },
+    {
+        lat: 20.3810, lng: -99.9750, intensidad: 0.3,
+        zona: "San Cayetano",
+        detalle: "Zona mencionada en reportes anteriores por riñas."
+    }
+];
